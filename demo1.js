361348c6911e8ee15a9b9d507de77ea6db5e077b
Postbird <ptbird@yeah.net>
2019-02-27 00:35:11 +0800

    feat: add demo1.ts

---
+++ demo1.js
@@ -0,0 +1,31 @@
+// 所有的验证器都放在一个文件里
+var lettersRegexp = /^[A-Za-z]+$/;
+var numberRegexp = /^[0-9]+$/;
+var LettersOnlyValidator = /** @class */ (function () {
+    function LettersOnlyValidator() {
+    }
+    LettersOnlyValidator.prototype.isAcceptable = function (s) {
+        return lettersRegexp.test(s);
+    };
+    return LettersOnlyValidator;
+}());
+var ZipCodeValidator = /** @class */ (function () {
+    function ZipCodeValidator() {
+    }
+    ZipCodeValidator.prototype.isAcceptable = function (s) {
+        return s.length === 5 && numberRegexp.test(s);
+    };
+    return ZipCodeValidator;
+}());
+// 一些简单的测试
+var strings = ["Hello", "8288", "123"];
+var validators = {};
+validators['ZIP code'] = new ZipCodeValidator();
+validators['Letters only'] = new LettersOnlyValidator();
+for (var _i = 0, strings_1 = strings; _i < strings_1.length; _i++) {
+    var s = strings_1[_i];
+    for (var name_1 in validators) {
+        var isMatch = validators[name_1].isAcceptable(s);
+        console.log("'" + s + "' " + (isMatch ? "matches" : "does not match") + " '" + name_1 + "'.");
+    }
+}
--- demo1.ts
+++ demo1.ts
@@ -2,21 +2,35 @@
 
 interface StringValidator {
     isAcceptable(s: string): boolean;
- }
+}
 
- let lettersRegexp = /^[A-Za-z]+$/;
- let numberRegexp = /^[0-9]+$/;
+let lettersRegexp = /^[A-Za-z]+$/;
+let numberRegexp = /^[0-9]+$/;
 
- class LettersOnlyValidator implements StringValidator {
-     isAcceptable(s: string) {
-         return lettersRegexp.test(s);
-     }
- }
+class LettersOnlyValidator implements StringValidator {
+    isAcceptable(s: string) {
+        return lettersRegexp.test(s);
+    }
+}
 
- class ZipCodeValidator implements StringValidator {
-     isAcceptable(s: string) {
-         return s.length === 5 && numberRegexp.test(s);
-     }
- }
+class ZipCodeValidator implements StringValidator {
+    isAcceptable(s: string) {
+        return s.length === 5 && numberRegexp.test(s);
+    }
+}
 
- 
+// 一些简单的测试
+
+let strings = ["Hello", "8288", "123"];
+
+let validators: { [s: string]: StringValidator; } = {};
+
+validators['ZIP code'] = new ZipCodeValidator();
+validators['Letters only'] = new LettersOnlyValidator();
+
+for (let s of strings) {
+    for (let name in validators) {
+        let isMatch = validators[name].isAcceptable(s);
+        console.log(`'${ s }' ${ isMatch ? "matches" : "does not match" } '${ name }'.`);
+    }
+}
