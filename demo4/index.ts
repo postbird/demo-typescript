bda702156c29eb4e0003ea1e1a5f5769340fc567
Postbird <ptbird@yeah.net>
2019-02-27 01:11:13 +0800

    feat: 别名

---
+++ demo4/index.ts
@@ -0,0 +1,12 @@
+// 别名
+
+namespace Shapes {
+    export namespace Polygons {
+        export class Triangle {  }
+        export class Square { }
+    }
+}
+
+import polygons = Shapes.Polygons;
+
+let sq = new polygons.Square(); // Same as "new Shapes.Polygons.Square()"
