8702139d6980a7b0f49c2ec2b7c9d30c5cbe9a36
Postbird <ptbird@yeah.net>
2019-02-27 00:19:20 +0800

    feat: init

---
+++ demo1.ts
@@ -0,0 +1,22 @@
+// 所有的验证器都放在一个文件里
+
+interface StringValidator {
+    isAcceptable(s: string): boolean;
+ }
+
+ let lettersRegexp = /^[A-Za-z]+$/;
+ let numberRegexp = /^[0-9]+$/;
+
+ class LettersOnlyValidator implements StringValidator {
+     isAcceptable(s: string) {
+         return lettersRegexp.test(s);
+     }
+ }
+
+ class ZipCodeValidator implements StringValidator {
+     isAcceptable(s: string) {
+         return s.length === 5 && numberRegexp.test(s);
+     }
+ }
+
+ 
