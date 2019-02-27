480d54a6a97d184d5ba32d165b09c25b19b253a8
Postbird <ptbird@yeah.net>
2019-02-28 00:45:06 +0800

    feat: add demo2

---
+++ demo2/shapes.js
@@ -0,0 +1,17 @@
+"use strict";
+exports.__esModule = true;
+var Shapes;
+(function (Shapes) {
+    var Triangle = /** @class */ (function () {
+        function Triangle() {
+        }
+        return Triangle;
+    }());
+    Shapes.Triangle = Triangle;
+    var Square = /** @class */ (function () {
+        function Square() {
+        }
+        return Square;
+    }());
+    Shapes.Square = Square;
+})(Shapes = exports.Shapes || (exports.Shapes = {}));
---
+++ demo2/shareCosumer.js
@@ -0,0 +1,4 @@
+"use strict";
+exports.__esModule = true;
+var shapes = require("./shapes");
+var t = new shapes.Shapes.Triangle();
